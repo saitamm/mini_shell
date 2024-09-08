737ad39fa9cf4e8da072ccf1ae6c8af4e09e19b0
Laila Elhouta <lai-elho@c3r3p6.1337.ma>
2024-09-08 18:32:37 +0100

    donne wa9ila

--- execution/exit.c
+++ execution/exit.c
@@ -6,21 +6,23 @@
 /*   By: lai-elho <lai-elho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 19:56:12 by lai-elho          #+#    #+#             */
-/*   Updated: 2024/09/08 14:04:42 by lai-elho         ###   ########.fr       */
+/*   Updated: 2024/09/08 18:30:55 by lai-elho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	isanumvalue(char *str)
+int isanumvalue(char *str)
 {
-	int		count;
-	size_t	number;
-	int		negative;
+	int count;
+	size_t number;
+	int negative;
 
 	negative = 0;
 	count = 0;
 	number = 0;
+	while (*str == ' ' || *str == '	')
+		str++;
 	if (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
@@ -39,17 +41,46 @@ int	isanumvalue(char *str)
 	return (1);
 }
 
+long ft_atoll(char *c)
+{
+	int i;
+	
+	int signe;
+	long result;
+
+	i = 0;
+	signe = 1;
+	result = 0;
+	// while ((c[i] <= 13 && c[i] >= 9) || c[i] == 32)
+	while (c[i] == ' ' || c[i] == '	')
+		i++;
+	if (c[i] == '+' || c[i] == '-')
+	{
+		if (c[i] == '-')
+			signe = -1;
+		i++;
+	}
+	while (c[i] <= '9' && c[i] >= '0')
+	{
+		result = result * 10 + (c[i] - '0');
+		i++;
+	}
+	return (result * signe);
+	// return (result);
+}
+
 void ft_exit(char **cmd)
 {
-    if(!cmd)
-        return;
-    if(cmd[1] && cmd[2] == NULL)
-    {	
-        if(isanumvalue(cmd[1]) )
-            exit(atoll(cmd[1])); // don't forget to change atoll to ft_....
-        else
-            printf("exit \nbash: exit: %s: numeric argument required\n",cmd[1]);
-			exit(2);
-    }else
+	if (!cmd)
+		return;
+	if (cmd[1] && cmd[2] == NULL)
+	{
+		if (isanumvalue(cmd[1]))
+			exit(ft_atoll(cmd[1])); // don't forget to change atoll to ft_....
+		else
+			printf("exit \nbash: exit: %s: numeric argument required\n", cmd[1]);
+		exit(2);
+	}
+	else
 		printf("exit \nbash: exit: too many arguments\n");
 }
